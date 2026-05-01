d12f159bf29b0c1546e48f08707616aec30ba628
moinkmu <m.a.khaja.moinuddin@accenture.com>
2026-05-01 10:25:09 +0530

    Add project files.

---
+++ DAL/Models/Appdb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Appdb : DbContext
+    {
+        public Appdb(DbContextOptions options) : base(options) { }  // db instnce
+
+        public DbSet<Users> Users { get; set; } // table
+        public DbSet<Tasks> Tasks { get; set; } // table
+    }
+}
---
+++ DAL/Models/Tasks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Tasks
+    {
+        [Key]
+        public int TaskID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime DueDate { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public string Priority { get; set; }
+    }
+}
