9dd07c68269fc1adc2ceb543e23bddfeec8d8baf
moinkmu <m.a.khaja.moinuddin@accenture.com>
2026-05-01 10:30:00 +0530

    login

---
+++ DAL/Irepo/IUsers.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Irepo
+{
+    public interface IUsers
+    {
+        Task AddUsers(Users data);
+        Task<bool> LoginUsers(LoginModel logindata);
+    }
+}
